4a27f7b63c67ceb295c80554ffa72083bc1a0d2c
Gonçalo Anacleto <goncalo.anacleto@sky.uk>
2022-10-07 17:58:12 +0100

    NEB-152 Unit Tests

--- src/test/java/com/nebula/nebulacarapp/NebulaCarappApplicationTests.java
+++ src/test/java/com/nebula/nebulacarapp/NebulaCarappApplicationTests.java
@@ -1,13 +1,29 @@
 package com.nebula.nebulacarapp;
 
+import com.nebula.nebulacarapp.controller.PrivateController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 class NebulaCarappApplicationTests {
 
+    @InjectMocks
+    private PrivateController privateController;
     @Test
-    void contextLoads() {
+    void clientCallsPrivateStatusReturns200(){
+        ResponseEntity<String> clientResponse = privateController.getStatus();
+        Assertions.assertAll(
+                () -> assertEquals(HttpStatus.OK, clientResponse.getStatusCode()),
+                () -> assertEquals("OK", clientResponse.getBody())
+        );
     }
 
 }
